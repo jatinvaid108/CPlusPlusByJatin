17f813bb1e97fc5f2a24d617d67f7fb0bfdff0c8
Jatin <jatinvaid00540@gmail.com>
2024-04-03 00:26:42 +0530

    New homework

---
+++ 8.Arrays/5HW.cpp
@@ -0,0 +1,52 @@
+// Given an integer n. Create an array containing squares of all natural numbers till n and print the elements of the array
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int num,square;
+    cout<<"Enter the number : "<<endl;
+    cin>>num;
+    int arr[num];
+    for(int i=1;i<=num;i++){
+        square=pow(i,2);
+        cout<<square<<" ";
+    }
+    
+}    
+
+// Given an array of integers, change the value of all odd indexed elements to its second multiple and increment all even indexed value by 10.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, i;
+    cout << "Enter the number of elements: ";
+    cin >> num;
+
+    int arr[num];
+
+    // Input array elements
+    for (i = 0; i < num; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    // Modify array elements based on index
+    for (i = 0; i < num; i++) {
+        if (i % 2 == 0)
+            arr[i] += 10; // Increment even-indexed elements by 10
+        else
+            arr[i] *= 2; // Multiply odd-indexed elements by 2
+    }
+
+    // Print modified array
+    cout << "Modified Array:" << endl;
+    for (i = 0; i < num; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
--- 8.Arrays/tempCodeRunnerFile.cpp
+++ 8.Arrays/tempCodeRunnerFile.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[]={-10,-5,-6,-2,-4,-6};
-    int n=sizeof(arr)/4;
-    int mx=INT16_MIN;                                      
-    for(int i=0;i<n;i++){                          //Here u can start with 0                  
-        // if (arr[i]>mx) mx=arr[i];
-        mx=max(mx,arr[i]);
+    int num,i;
+    cout<<"Enter the number : "<<endl;
+    cin>>num;
+    int even;
+    int arr[num];
+    for(i=0;i<num;i++){
+        if(i%2==0) arr[i]+=10;
+        else arr[i]*=2 ;
     }
-    cout<<"The greatest element in array is :-  "<<mx;
-}
+    for(i=0;i<num;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+}  
