2c3a8a24596d031b18ed20d8b9216d4fff1d4e27
Jatin <jatinvaid00540@gmail.com>
2024-05-04 01:07:50 +0530

    Deleted some extra files

--- 1.Basics/8Typecasting.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int x;
-    scanf("%d", &x);           // Taking integer input
-    float y = (float)x;        // Typecasting x to float
-    printf("%f\n", y / 2);     // Printing y divided by 2
-    return 0;
-}
